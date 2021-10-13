b1f5dd11c1645330574c450dad6cfc6fdd9f205e
Penguin-God <gkswh4860@naver.com>
2021-10-13 13:07:40 +0900

    포인터 프로젝트 끝

--- pointer_project.c
+++ pointer_project.c
@@ -4,7 +4,6 @@
 void InitData();
 void ShowFishsStatus();
 void DecreaseWater(long time);
-void IncreaseWater(int num);
 void LevelUp(long time);
 void Clear();
 void CheckFishStatus();
@@ -29,56 +28,39 @@ int main(void) {
 	cursor = arr_Fishs;
 
 	start_Time = clock(); // 현재 시간을 milli second (1000분의 1초)로 반환
+
+	// 루트는 함수 내 exit에서 끝나서 전체 프로그램도 같이 끝남
 	while (1) {
+		// 최근 경과 시간 경신을 위한 변수
 		int previous_total_elapesd_time = total_elapesd_time;
-		CheckFishStatus();
-		LevelUp(total_elapesd_time);
-
-		int deadCount = 0;
-		for (int i = 0; i < 6; i++) {
-			if (arr_Fishs[i] > 0) break;
-			deadCount++;
-		}
-		if (deadCount >= 6) break;
-
-		if (level >= 6) {
-			Clear();
-			break;
-		}
 
 		ShowFishsStatus();
 		printf("몇 번 어항에 물을 주시겠습니까?\n");
 		scanf_s("%d", &getNum);
 
+		// 입력값이 범위를 벗어났다면
 		if (getNum > 6 || getNum < 1) {
 			printf("입력값이 잘못돼었습니다.\n");
 			continue;
-		}
-		if (arr_Fishs[getNum - 1] <= 0) {
+		} // 입력값을 정상인데 그 물고기가 죽었다면
+		else if (cursor[getNum - 1] <= 0) {
 			printf("그 물고기는 죽었어!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
 			continue;
 		}
-
-		IncreaseWater(getNum);
+		cursor[getNum - 1]++; // 물주기
 
 		total_elapesd_time = (clock() - start_Time) / CLOCKS_PER_SEC;
 		printf("총 경과 시간 : %d초\n", total_elapesd_time);
 		
-		// 직전 물 준 시간(마지막으로 물 준 시간 이후 흐른 시간)
+		// 직전 물 준 시간(마지막으로 물 준 시간 이후 흐른 시간) 
+		// 물 주기 전에 previous_total_elapesd_time 을 갱신하고 물 흐른 뒤에 시간에 빼서 시간 구함
 		previous_elapesd_time = total_elapesd_time - previous_total_elapesd_time;
 		printf("최근 경과 시간 : %d초\n", previous_elapesd_time);
 
 		DecreaseWater(previous_elapesd_time);
+		CheckFishStatus();
+		LevelUp(total_elapesd_time);
 	}
-
-	if (isClear == 1) printf("ㅊㅋ");
-	else printf("털렸구나아~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
-
-	getchar();
-	getchar();
-	getchar();
-	getchar();
-
 	return 0;
 }
 
@@ -104,36 +86,49 @@ void ShowFishsStatus() {
 
 void DecreaseWater(long time) {
 	for (int i = 0; i < 6; i++) {
-		arr_Fishs[i] -= time * level * 3;
+		arr_Fishs[i] -= (int)time * level * 4;
 	}
-}
-
-void IncreaseWater(int num) {
-	arr_Fishs[num - 1]++;
 }
 
 void LevelUp(long time) {
 	if (time > level * 3) { 
 		level++;
-		if (level <= 5)
-			printf("\n 와!!! 레밸업 아시는구나!!!! %d레밸 아시는구나!!!!\n", level);
+		if (level < 5) printf("\n 와!!! 레밸업 아시는구나!!!! %d레밸 아시는구나!!!!\n", level);
+		else Clear();
 	}
 }
 
 void CheckFishStatus() {
+	int deadCount = 0;
 	for (int i = 0; i < 6; i++) {
-		if (arr_Fishs[i] <= 0 && arr_Fishs_Daed[i] == 0) {
-			printf("%d번 물고기 컷!!! 컷컷컷!!!\n", i + 1);
-			arr_Fishs_Daed[i] = 1;
+		if (arr_Fishs[i] <= 0) {
+			if (arr_Fishs_Daed[i] == 0) {
+				printf("%d번 물고기 컷!!! 컷컷컷!!!\n", i + 1);
+				arr_Fishs_Daed[i] = 1;
+			}
+			deadCount++;
 		}
+	}
+
+	if (deadCount >= 6) {
+		printf("털렸구나아~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+		getchar(); // 게임이 갑자기 끝나는거 방지용
+		getchar();
+		getchar();
+		exit();
 	}
 }
 
 void Clear() {
-	printf("\n\n\n");
+	printf("\n\n");
+	printf("\n 와!!! 레밸업 끝!!! 맥스 레벨 아시는구나!!!!\n");
 	for (int i = 0; i < 6; i++) {
-		if (arr_Fishs_Daed[i] == 0)
+		if (arr_Fishs[i] > 0)
 			printf("%d번 물고기 스틸 얼라이브\n", i + 1);
 	}
-	isClear = 1;
+	printf("ㅊㅋ");
+	getchar();
+	getchar();
+	getchar();
+	exit();
 }
